61d2f6475785f6641e936c001dd8e724594ccac3
Sanil-Surve <sanilsurve42@gmail.com>
2024-12-05 19:28:17 +0530

    intial commit

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/api/generateQuizzes', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
